93c671b68f6bd055664a32ba04501f66b8bf8ab2
saiki1530 <quang.sang1530@gmail.com>
2023-10-05 19:40:04 +0700

    layout pass1

---
+++ resources/views/layouts/menuAD.blade.php
@@ -0,0 +1,38 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="/admin/dashboard">
+                <i class="mdi mdi-home menu-icon"></i>
+                <span class="menu-title">Bảng điều khiển</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <i class="mdi mdi-circle-outline menu-icon"></i>
+                <span class="menu-title">Quản lý</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="{{route('admin.allsp')}}">Quản lý sản phẩm</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="../../admin/allus">Quản Lý người dùng</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="{{route('admin.alldm')}}">Quản Lý danh mục</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="{{route('admin.allbl')}}">Quản lý bình luận</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="{{route('admin.alltg')}}">Quản Lý tác giả</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/admin/allorder">
+                <i class="mdi mdi-view-headline menu-icon"></i>
+                <span class="menu-title">Đơn hàng</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="pages/charts/chartjs.html">
+                <i class="mdi mdi-chart-pie menu-icon"></i>
+                <span class="menu-title">Biểu đồ</span>
+            </a>
+        </li>
+    </ul>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [ProductController::class, 'index']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
